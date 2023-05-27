aabe2cae0d8bd6c5a860ed9b5ed09033c89775fb
bj990520 <bg63645539@gmail.com>
2023-05-27 21:15:17 +0900

    sound

--- Assets/02.Scripts/Particle.cs
+++ Assets/02.Scripts/Particle.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class Particle : MonoBehaviour
 {
     public GameObject coin;
+    public float destroyDelay = 2.0f;
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             Debug.Log("캐릭터 코인과 충돌");
-            Instantiate(coin, transform.position, Quaternion.identity);
+            GameObject particle =  Instantiate(coin, transform.position, Quaternion.identity);
+
+            Destroy(particle, destroyDelay);
+
             Destroy(gameObject);
         }
+
     }
 }
